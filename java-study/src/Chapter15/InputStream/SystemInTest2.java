3291ccafc95f9c7e7d8256f8f920ac75bbdce18a
JUNGSANGYUN <sdsd98987@gmail.com>
2023-05-29 23:42:01 +0900

    [자바] 입출력정리

---
+++ java-study/src/Chapter15/InputStream/SystemInTest2.java
@@ -0,0 +1,22 @@
+package Chapter15.InputStream;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SystemInTest2 {
+    public static void main(String[] args) {
+
+        System.out.println("입력 후 '끝' 이라고 쓰세요 : ");
+
+        try {
+            int i;
+            InputStreamReader isr = new InputStreamReader(System.in);
+            while ((i = isr.read()) != '끝'){
+                System.out.print((char) i);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
